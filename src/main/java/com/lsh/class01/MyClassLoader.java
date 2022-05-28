21bab0fe10f5ed6f2f21425230993bdcbee18b11
Liu-Shihao <liush99@foxmail.com>
2022-05-29 00:36:47 +0800

    自定义类加载器重写findClass()方法，一直从APPClassLoader类加载器加载，不能从自定义的来加载器进行加载TODO

--- src/main/java/com/lsh/class01/MyClassLoader.java
+++ src/main/java/com/lsh/class01/MyClassLoader.java
@@ -8,40 +8,60 @@ import java.io.FileInputStream;
  * @author ：LiuShihao
  * @date ：Created in 2022/5/28 4:39 下午
  * @desc ：自定义类加载器 继承java.lang.ClassLoader
- * 重写findClass（）方法
+ * 重写 findClass（）方法
  * 使用该类加载器时调用loadClass（）方法
+ * Users/LiuShihao/Desktop/Demo.class
  */
 public class MyClassLoader extends ClassLoader {
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        ///Users/LiuShihao/IdeaProjects/jvm-demo/target/classes/com/lsh/class01/HelloWorld.class
-        String s = "/Users/LiuShihao/IdeaProjects/jvm-demo/target/classes/" + name.replaceAll("\\.", "/");
+        System.out.println("执行了MyClassLoader的findClass（）方法");
+//        String s = "/Users/LiuShihao/IdeaProjects/jvm-demo/target/classes/" + name.replaceAll("\\.", "/")+".class";
+        String s = "/Users/LiuShihao/Desktop/Demo.class";
+
+        System.out.println("name:"+name);
         System.out.println(s);
+
         File file = new File(s);
+        if (!file.exists()){
+            System.out.println("File Not Fount:"+name);
+            return super.findClass(name);
+        }
         try {
             FileInputStream fis = new FileInputStream(file);
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            int b = 0;
-            while ((b = fis.read()) != 0){
-                bos.write(b);
+            byte[] bytes = new byte[fis.available()];
+            int length = 0;
+            if ((length = fis.read(bytes))!= -1){
+                bos.write(bytes,0,length);
             }
-            byte[] bytes = bos.toByteArray();
-            fis.close();
-            bos.close();
-            return defineClass(name,bytes,0,bytes.length);
+            System.out.println("____"+name);
+            return defineClass(name,bos.toByteArray(),0,bos.toByteArray().length);
         }catch (Exception e){
             e.printStackTrace();
         }
         return super.findClass(name);
     }
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static void main(String[] args) throws ClassNotFoundException {
         MyClassLoader myClassLoader = new MyClassLoader();
-        Class<?> aClass = myClassLoader.loadClass("com.lsh.class01.HelloWorld");
-        HelloWorld o = (HelloWorld)aClass.newInstance();
-        o.m();
-        System.out.println(myClassLoader.getClass().getClassLoader());
-        System.out.println(myClassLoader.getParent());
+//        Class<?> aClass = myClassLoader.loadClass("com.lsh.class01.Demo");
+        Class<?> aClass = myClassLoader.loadClass("Demo");
+        System.out.println(aClass.getClassLoader());
+
+        myClassLoader = new MyClassLoader();
+//        Class<?> aClass1 = myClassLoader.loadClass("com.lsh.class01.Demo");
+        Class<?> aClass1 = myClassLoader.loadClass("Demo");
+        System.out.println(aClass.getClassLoader());
+        System.out.println(aClass == aClass1);//判断加载出的类是否是同一个类
+
+//        Demo o = (Demo)aClass.newInstance();
+//        o.m();
+
+
+//        Class<?> aClass1 = myClassLoader.loadClass("com.lsh.class01.Demo");
+
+
 
     }
 
