d73f5cc4eb14e70537f22c08fc6edffd5f0bd166
Liu-Shihao <liush99@foxmail.com>
2022-05-29 00:45:57 +0800

    update

--- src/main/java/com/lsh/class01/Demo.java
+++ src/main/java/com/lsh/class01/Demo.java
@@ -2,8 +2,12 @@ package com.lsh.class01;
 
 /**
  * @author ：LiuShihao
- * @date ：Created in 2022/5/27 11:54 下午
+ * @date ：Created in 2022/5/28 10:45 下午
  * @desc ：
  */
 public class Demo {
+
+    public static void m(){
+        System.out.println("Hello JVM");
+    }
 }
