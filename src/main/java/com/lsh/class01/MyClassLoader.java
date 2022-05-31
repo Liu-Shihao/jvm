bc8aebf8738e11fa59050cde777d9479a76045fc
Liu-Shihao <liush99@foxmail.com>
2022-05-31 21:46:13 +0800

    使用自定义类类加载器加载与类加载器的双亲委派机制

--- src/main/java/com/lsh/class01/MyClassLoader.java
+++ src/main/java/com/lsh/class01/MyClassLoader.java
@@ -12,6 +12,24 @@ import java.io.FileInputStream;
  * 使用该类加载器时调用loadClass（）方法
  * 类路径目录：/Users/LiuShihao/IdeaProjects/jvm-demo/target/classes/com/lsh/class01/Demo.class
  * 其他目录：Users/LiuShihao/Desktop/Demo.class
+ *
+ * 注意：
+ * 当class文件从类路径移动到桌面的时候，需要将java文件 上的package 表示当前类路径的代码注释掉之后重新编译
+ * 将重新编译后的字节码文件移动到桌面，则才能实现从自定义类加载器进行加载，
+ * 否则当类路径中存在这个class文件的时候，由于父类委托机制会将加载类的向上委托，所以父类加载器AppClassLoader检查到自己可以进行加载，就直接将类进行加载了
+ *
+ * 执行了MyClassLoader的findClass（）方法
+ * name:Demo
+ * /Users/LiuShihao/Desktop/Demo.class
+ * ____Demo
+ * com.lsh.class01.MyClassLoader@511d50c0
+ * 执行了MyClassLoader的findClass（）方法
+ * name:Demo
+ * /Users/LiuShihao/Desktop/Demo.class
+ * ____Demo
+ * com.lsh.class01.MyClassLoader@511d50c0
+ * false
+ *
  */
 public class MyClassLoader extends ClassLoader {
     @Override
@@ -54,7 +72,7 @@ public class MyClassLoader extends ClassLoader {
 //        Class<?> aClass1 = myClassLoader.loadClass("com.lsh.class01.Demo");
         Class<?> aClass1 = myClassLoader.loadClass("Demo");
         System.out.println(aClass.getClassLoader());
-        System.out.println(aClass == aClass1);//判断加载出的类是否是同一个类
+        System.out.println(aClass == aClass1);//判断加载出的类是否是同一个类  由于存在双亲委派机制，可以保证类在JVM中的唯一性
 //        Demo o = (Demo)aClass.newInstance();
 //        o.m();
 //        Class<?> aClass1 = myClassLoader.loadClass("com.lsh.class01.Demo");
